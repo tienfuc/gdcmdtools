d25fcf60a3791ef04457654c5116bce98fce2c12
ctf <tienfu.c@gmail.com>
2014-12-17 17:15:28 +0800

    added help text for export format

--- gdget.py
+++ gdget.py
@@ -3,6 +3,7 @@
 
 
 import sys
+import re
 from gdcmdtools.base import BASE_INFO
 from gdcmdtools.base import DEBUG_LEVEL 
 
@@ -21,12 +22,20 @@ __THIS_APP = 'gdget'
 __THIS_DESCRIPTION = 'Tool to download file from Google Drive'
 __THIS_VERSION = BASE_INFO["version"]
 
+export_format = {
+    "application/vnd.google-apps.spreadsheet":["pdf", "ods", "xlsx"],
+    "application/vnd.google-apps.document":["pdf", "docx", "rtf", "odt", "html", "txt"],    
+    "application/vnd.google-apps.presentation":["pdf", "pptx", "txt"],
+    "application/vnd.google-apps.drawing":["png", "pdf", "jpeg", "svg"],
+    }
+
+
 
 def test():
     assert True
 
 if __name__ == '__main__':
-
+    
     arg_parser = argparse.ArgumentParser( \
             description='%s v%s - %s - %s (%s)' % 
             (__THIS_APP, __THIS_VERSION, __THIS_DESCRIPTION, BASE_INFO["app"], BASE_INFO["description"]),
@@ -34,7 +43,9 @@ if __name__ == '__main__':
 
     arg_parser.add_argument('file_id', help='The id for the file you\'re going to download')
     
-    arg_parser.add_argument('-f', '--export_format', metavar='FORMAT', default='raw', required=False, help='specify the format for downloading') 
+    help_export_format = "\n".join([ re.search(".*google-apps\.(.*)", k ).group(1)+": "+", ".join(export_format[k]) for k in export_format.iterkeys() ])
+
+    arg_parser.add_argument('-f', '--export_format', metavar='FORMAT', default='raw', required=False, help='specify the format for downloading,\ngoogle_format: export_format\n%s' % help_export_format) 
     arg_parser.add_argument('-s', '--save_as', metavar='NEW_FILE_NAME', help='save the downloaded file as ') 
 
     arg_parser.add_argument('--debug', choices=DEBUG_LEVEL, default=DEBUG_LEVEL[-1],
@@ -45,7 +56,6 @@ if __name__ == '__main__':
 
     # set debug devel
     logger.setLevel(getattr(logging, args.debug.upper()))
-
     logger.debug(args)
 
     get = GDGet(args.file_id, args.export_format, args.save_as)
