9c04ac4921e76993cb00096b89bd4854fbe09e83
ctf <tienfu.c@gmail.com>
2014-12-18 12:23:08 +0800

    parse_gas_json to download the gas project

--- gdcmdtools/get.py
+++ gdcmdtools/get.py
@@ -12,8 +12,32 @@ from gdcmdtools.auth import GDAuth
 import requests
 import re
 import os
+import json
 
 import pprint
+
+export_format = {
+    "application/vnd.google-apps.spreadsheet":["pdf", "ods", "xlsx"],
+    "application/vnd.google-apps.document":["pdf", "docx", "rtf", "odt", "html", "txt"],    
+    "application/vnd.google-apps.presentation":["pdf", "pptx", "txt"],
+    "application/vnd.google-apps.drawing":["png", "pdf", "jpeg", "svg"],
+    "application/vnd.google-apps.script+json":["js"],
+    }
+
+""" application/vnd.google-apps.script+json
+{  
+    "files": [  
+        {  
+            "id": "bbda34aa-c700-48d9-88bd-ad2573a0620a",  
+            "name": "Code",  
+            "source": "FILE CONTENT",
+            "type": "server_js"
+        }
+    ]
+}
+
+
+"""
 
 class GDGet:
     def __init__(self, file_id, format, save_as):
@@ -36,6 +60,37 @@ class GDGet:
         else:
             self.save_as = os.path.abspath(save_as)
  
+    def parse_gas_json(self, file_content, save_as):
+        map_type_ext = {"server_js":"js", "html":"html"}
+        try:
+            jsons = json.loads(file_content)
+            new_json = {"files":[]}
+            for j in jsons["files"]:
+                file_id = j["id"]
+                file_name = j["name"]
+                file_source = j["source"]
+                file_type = j["type"]
+
+                if file_type in map_type_ext.keys():
+                    file_ext = map_type_ext[file_type]
+                else:
+                    file_ext = file_type
+
+                file_fullname = "%s.%s" % (file_name, file_ext) 
+
+                with open(file_fullname, 'wb+') as f:
+                    f.write(file_source)
+                
+                j.pop("source")
+                new_json["files"].append(j)
+
+            with open(save_as, 'wb+') as f:
+                f.write(json.dumps(new_json, indent=4))
+
+        except Exception, e:  
+            logger.error(e)
+            raise
+
     def run(self):
         try: 
             service_response = self.get()
@@ -55,8 +110,11 @@ class GDGet:
             if self.save_as == None:
                self.save_as = title 
 
-            with open(self.save_as, 'wb+') as f:
-                f.write(file_content)
+            if self.format == "json":
+                self.parse_gas_json(file_content, self.save_as)               
+            else:
+                with open(self.save_as, 'wb+') as f:
+                    f.write(file_content)
 
         except Exception, e:
             logger.error(e)
@@ -68,6 +126,9 @@ class GDGet:
     def get(self):
         try:
             response = self.service.files().get(fileId=self.file_id).execute()
+            # response = self.service.files(q="mimeType%3D'application%2Fvnd.google-apps.script'").execute()
+            #response = self.service.files().list(q="mimeType=application/vnd.google-apps.script").execute()
+            #response = self.service.files().list().execute()
             logger.debug(pprint.pformat(response))
             return response
 
@@ -82,6 +143,10 @@ class GDGet:
 
         title = service_response.get('title',None)
 
+        
+        logger.debug(title)
+        logger.debug(export_links)
+
         if export_links == None:
             download_link = service_response.get(u'downloadUrl', None)
             return_format["raw"] = download_link
@@ -90,7 +155,8 @@ class GDGet:
 
             if len(export_link_values) > 0 :
                 for link in export_link_values:
-                    m = re.match(r'^.*exportFormat=(.*)$',link)
+                    #m = re.match(r'^.*exportFormat=(.*)$',link)
+                    m = re.match(r'^.*[Ff]ormat=(.*)$',link)
                     return_format[m.group(1)] = link
 
         return title, return_format
--- gdget.py
+++ gdget.py
@@ -8,6 +8,7 @@ from gdcmdtools.base import BASE_INFO
 from gdcmdtools.base import DEBUG_LEVEL 
 
 from gdcmdtools.get import GDGet
+from gdcmdtools.get import export_format
 
 import argparse
 from argparse import RawTextHelpFormatter
@@ -22,14 +23,16 @@ __THIS_APP = 'gdget'
 __THIS_DESCRIPTION = 'Tool to download file from Google Drive'
 __THIS_VERSION = BASE_INFO["version"]
 
-export_format = {
-    "application/vnd.google-apps.spreadsheet":["pdf", "ods", "xlsx"],
-    "application/vnd.google-apps.document":["pdf", "docx", "rtf", "odt", "html", "txt"],    
-    "application/vnd.google-apps.presentation":["pdf", "pptx", "txt"],
-    "application/vnd.google-apps.drawing":["png", "pdf", "jpeg", "svg"],
-    }
+# u'exportLinks': {u'application/vnd.google-apps.script+json': u'https://script.google.com/feeds/download/export?id=1u3Im-CCiTDpaKpJ6JqMgDRxKoi2TdCdaHAg65jcseyL-x0NSL7dRZs7z&format=json'}
 
-
+"""
+u'exportLinks': {u'application/pdf': u'https://docs.google.com/feeds/download/presentations/Export?id=1lxwOegts6hKssHtjH6
+Dj0WPbwlF71KhFGLSnCtruraY&exportFormat=pdf',
+                  u'application/vnd.openxmlformats-officedocument.presentationml.presentation': u'https://docs.google.com/
+                  feeds/download/presentations/Export?id=1lxwOegts6hKssHtjH6Dj0WPbwlF71KhFGLSnCtruraY&exportFormat=pptx',
+                                    u'text/plain': u'https://docs.google.com/feeds/download/presentations/Export?id=1lxwOegts6hKssHtjH6Dj0WP
+                                    bwlF71KhFGLSnCtruraY&exportFormat=txt'}
+"""
 
 def test():
     assert True
@@ -45,7 +48,7 @@ if __name__ == '__main__':
     
     help_export_format = "\n".join([ re.search(".*google-apps\.(.*)", k ).group(1)+": "+", ".join(export_format[k]) for k in export_format.iterkeys() ])
 
-    arg_parser.add_argument('-f', '--export_format', metavar='FORMAT', default='raw', required=False, help='specify the format for downloading,\ngoogle_format: export_format\n%s' % help_export_format) 
+    arg_parser.add_argument('-f', '--export_format', metavar='FORMAT', default='raw', required=False, help='specify the export format for downloading,\ngoogle_format: export_format\n%s' % help_export_format) 
     arg_parser.add_argument('-s', '--save_as', metavar='NEW_FILE_NAME', help='save the downloaded file as ') 
 
     arg_parser.add_argument('--debug', choices=DEBUG_LEVEL, default=DEBUG_LEVEL[-1],
