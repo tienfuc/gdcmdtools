3728efce83810146b757194d651ec73b07ead3a2
ctf <tienfu.c@gmail.com>
2015-03-20 11:20:15 +0800

    upgrade version

--- gdcmdtools/base.py
+++ gdcmdtools/base.py
@@ -18,7 +18,7 @@ logger.setLevel(logging.INFO)
 BASE_INFO = {
         "app":"gdcmdtools",
         "description":'Google Drive command line tools',
-        "version":'0.93'}
+        "version":'0.94'}
 
 GDAPI_VER = 'v2'
 FTAPI_VER = 'v1'
