a45831ab7b4f9744307bc4fdb286e2d9d94003b5
tienfuc <tienfu.c@gmail.com>
2015-12-28 11:35:48 +0800

    pep8 on test.py

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ class Test(unittest.TestCase):
     def setUp(self):
         return True
 
-
     def test_00_if_travis(self):
         if os.environ.get('TRAVIS', None) == "true":
             Test.if_travis = True
@@ -22,65 +21,93 @@ class Test(unittest.TestCase):
         else:
             Test.if_travis = False
             Test.folder_id = "0B60IjoJ-xHK6Rl9zMkVlNE1scTQ"
-    
 
     def test_01_get_secret(self):
-        if Test.if_travis == False:
+        if not Test.if_travis:
             return True
 
         client_id = os.environ['client_id']
         client_secret = os.environ['client_secret']
-        
+
         secret_example_file = "./samples/gdcmdtools.secrets.EXAMPLE"
-        
+
         with open(secret_example_file) as f:
             secret_example = f.read()
-            
-        new_secret_1 = re.sub(r'"client_id":"X"',r'"client_id":"%s"' % client_id, secret_example, flags=re.MULTILINE)
-        new_secret = re.sub(r'"client_secret":"X"',r'"client_secret":"%s"' % client_secret, new_secret_1, flags=re.MULTILINE)
+
+        new_secret_1 = re.sub(
+            r'"client_id":"X"',
+            r'"client_id":"%s"' %
+            client_id,
+            secret_example,
+            flags=re.MULTILINE)
+        new_secret = re.sub(
+            r'"client_secret":"X"',
+            r'"client_secret":"%s"' %
+            client_secret,
+            new_secret_1,
+            flags=re.MULTILINE)
 
         Test.secret_file = os.path.expanduser("~/.gdcmdtools.secrets")
 
-        with open(Test.secret_file,'w') as f:
+        with open(Test.secret_file, 'w') as f:
             f.write(new_secret)
 
         assert True
 
-
     def test_01_get_credentials(self):
-        if Test.if_travis == False:
+        if not Test.if_travis:
             return True
 
         client_id = os.environ['client_id']
         client_secret = os.environ['client_secret']
         access_token = os.environ['access_token']
         refresh_token = os.environ['refresh_token']
-        
+
         credentials_example_file = "./samples/gdcmdtools.creds.EXAMPLE"
-        
+
         with open(credentials_example_file) as f:
             credentials_example = f.read()
-            
-        new_credentials_id = re.sub(r'"client_id":"X"',r'"client_id":"%s"' % client_id, credentials_example, flags=re.MULTILINE)
-        new_credentials_secret = re.sub(r'"client_secret":"X"',r'"client_secret":"%s"' % client_secret, new_credentials_id, flags=re.MULTILINE)
-        new_credentials_token = re.sub(r'"access_token":"X"',r'"access_token":"%s"' % access_token, new_credentials_secret, flags=re.MULTILINE)
-        new_credentials = re.sub(r'"refresh_token":"X"',r'"refresh_token":"%s"' % refresh_token, new_credentials_token, flags=re.MULTILINE)
+
+        new_credentials_id = re.sub(
+            r'"client_id":"X"',
+            r'"client_id":"%s"' %
+            client_id,
+            credentials_example,
+            flags=re.MULTILINE)
+        new_credentials_secret = re.sub(
+            r'"client_secret":"X"',
+            r'"client_secret":"%s"' %
+            client_secret,
+            new_credentials_id,
+            flags=re.MULTILINE)
+        new_credentials_token = re.sub(
+            r'"access_token":"X"',
+            r'"access_token":"%s"' %
+            access_token,
+            new_credentials_secret,
+            flags=re.MULTILINE)
+        new_credentials = re.sub(
+            r'"refresh_token":"X"',
+            r'"refresh_token":"%s"' %
+            refresh_token,
+            new_credentials_token,
+            flags=re.MULTILINE)
 
         Test.credentials_file = os.path.expanduser("~/.gdcmdtools.creds")
 
-        with open(Test.credentials_file,'w') as f:
+        with open(Test.credentials_file, 'w') as f:
             f.write(new_credentials)
-        
+
         assert True
 
-
     def test_10_raw_put(self):
-        files = {"./samples/sample.txt":0, "":2, "x":1}
+        files = {"./samples/sample.txt": 0, "": 2, "x": 1}
 
         for file, code in files.iteritems():
-            cmd_debug = "python ./gdput.py --debug debug -f %s -t raw %s" % (Test.folder_id, file)
-            print "Run %s> %s" % ("-"*30, cmd_debug)
-            
+            cmd_debug = "python ./gdput.py --debug debug -f %s -t raw %s" % (
+                Test.folder_id, file)
+            print "Run %s> %s" % ("-" * 30, cmd_debug)
+
             try:
                 response = subprocess.check_output(cmd_debug, shell=True)
                 m = re.search("id: (.*)", response, re.MULTILINE)
@@ -94,18 +121,18 @@ class Test(unittest.TestCase):
                 else:
                     assert False
 
-            except subprocess.CalledProcessError, e:
-                assert (e.returncode == code) 
-
+            except subprocess.CalledProcessError as e:
+                assert (e.returncode == code)
 
     def test_11_raw_get(self):
-        files = {Test.raw_file_id:0, "":2, "x":1}
+        files = {Test.raw_file_id: 0, "": 2, "x": 1}
 
         for file_id, code in files.iteritems():
             file_ori = file_id
             file_get = "/tmp/gdcmdtools.tmp"
-            cmd_debug = "python ./gdget.py --debug debug -f raw -s %s %s" % (file_get, file_id)
-            print "Run %s> %s" % ("-"*30, cmd_debug)
+            cmd_debug = "python ./gdget.py --debug debug -f raw -s %s %s" % (
+                file_get, file_id)
+            print "Run %s> %s" % ("-" * 30, cmd_debug)
 
             try:
                 response = subprocess.check_output(cmd_debug, shell=True)
@@ -113,19 +140,19 @@ class Test(unittest.TestCase):
                     result = filecmp.cmp(file_ori, file_get)
                     assert result
 
-            except subprocess.CalledProcessError, e:
+            except subprocess.CalledProcessError as e:
                 assert (e.returncode == code)
-
 
     def test_12_converted_put(self):
         file = "./samples/sample.txt"
         cmd = "python ./gdput.py -t doc %s" % file
-        cmd_debug = "python ./gdput.py --debug debug -f %s -t doc %s" % (Test.folder_id, file)
-        print "Run %s> %s" % ("-"*30, cmd_debug)
-        
+        cmd_debug = "python ./gdput.py --debug debug -f %s -t doc %s" % (
+            Test.folder_id, file)
+        print "Run %s> %s" % ("-" * 30, cmd_debug)
+
         try:
             response = subprocess.check_output(cmd_debug, shell=True)
-        except subprocess.CalledProcessError, e:
+        except subprocess.CalledProcessError as e:
             assert e.returncode
 
         m = re.search("id: (.*)", response, re.MULTILINE)
@@ -138,16 +165,16 @@ class Test(unittest.TestCase):
         else:
             assert False
 
-
     def test_20_gas_put(self):
         file = "./samples/gas/gas.json"
         cmd = "python ./gdput.py -t gas --gas_new %s" % file
-        cmd_debug = "python ./gdput.py --debug debug -t gas -f %s --gas_new %s" % (Test.folder_id, file)
-        print "Run %s> %s" % ("-"*30, cmd_debug)
-        
+        cmd_debug = "python ./gdput.py --debug debug -t gas -f %s --gas_new %s" % (
+            Test.folder_id, file)
+        print "Run %s> %s" % ("-" * 30, cmd_debug)
+
         try:
             response = subprocess.check_output(cmd_debug, shell=True)
-        except subprocess.CalledProcessError, e:
+        except subprocess.CalledProcessError as e:
             assert e.returncode
 
         m = re.search("id: (.*)", response, re.MULTILINE)
@@ -159,7 +186,6 @@ class Test(unittest.TestCase):
         else:
             assert False
 
-
     def test_21_gas_get(self):
 
         assert Test.gas_file_id
@@ -167,12 +193,13 @@ class Test(unittest.TestCase):
         if Test.gas_file_id:
             file_ori = "./samples/gas/Code.js"
             file_get = "./Code.js"
-            cmd_debug = "python ./gdget.py --debug debug -f json %s" % (Test.gas_file_id)
-            print "Run %s> %s" % ("-"*30, cmd_debug)
+            cmd_debug = "python ./gdget.py --debug debug -f json %s" % (
+                Test.gas_file_id)
+            print "Run %s> %s" % ("-" * 30, cmd_debug)
 
             try:
                 response = subprocess.check_output(cmd_debug, shell=True)
-            except subprocess.CalledProcessError, e:
+            except subprocess.CalledProcessError as e:
                 assert e.returncode
 
             result = filecmp.cmp(file_ori, file_get)
@@ -181,18 +208,18 @@ class Test(unittest.TestCase):
         else:
             assert False
 
-
     def test_22_cp(self):
 
         assert Test.gas_file_id
 
         if Test.gas_file_id:
-            cmd_debug = "python ./gdcp.py --debug debug -f %s %s" % (Test.folder_id, Test.gas_file_id)
-            print "Run %s> %s" % ("-"*30, cmd_debug)
+            cmd_debug = "python ./gdcp.py --debug debug -f %s %s" % (
+                Test.folder_id, Test.gas_file_id)
+            print "Run %s> %s" % ("-" * 30, cmd_debug)
 
             try:
                 response = subprocess.check_output(cmd_debug, shell=True)
-            except subprocess.CalledProcessError, e:
+            except subprocess.CalledProcessError as e:
                 assert e.returncode
 
             m = re.search("id: (.*)", response, re.MULTILINE)
@@ -203,48 +230,45 @@ class Test(unittest.TestCase):
             else:
                 assert False
 
-
         else:
             assert False
 
-
     def test_30_mkdir(self):
         dir_name = "a dir"
-        cmd_debug = "python ./gdmkdir.py --debug debug -f %s \"%s\"" % (Test.folder_id, dir_name)
-        print "Run %s> %s" % ("-"*30, cmd_debug)
+        cmd_debug = "python ./gdmkdir.py --debug debug -f %s \"%s\"" % (
+            Test.folder_id, dir_name)
+        print "Run %s> %s" % ("-" * 30, cmd_debug)
 
         try:
             response = subprocess.check_output(cmd_debug, shell=True)
-        except subprocess.CalledProcessError, e:
+        except subprocess.CalledProcessError as e:
             assert e.returncode
 
         m = re.search("id: (.*)", response, re.MULTILINE)
 
-        assert m 
-        
+        assert m
+
         if m:
             Test.files_rm.append(m.group(1))
             assert True
         else:
             assert False
 
-
     def test_90_rm(self):
         for file in self.files_rm:
             cmd_debug = "python ./gdrm.py --debug debug %s" % file
-            print "Run %s> %s" % ("-"*30, cmd_debug)
+            print "Run %s> %s" % ("-" * 30, cmd_debug)
 
             try:
                 response = subprocess.check_output(cmd_debug, shell=True)
-            except subprocess.CalledProcessError, e:
+            except subprocess.CalledProcessError as e:
                 assert e.returncode
 
             assert True
 
-
     def test_99_cleanup(self):
         return True
 
-    
+
 if __name__ == '__main__':
     unittest.main()
