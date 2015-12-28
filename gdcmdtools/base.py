dc57f4351139f9a280dbd909d67e189063cf2ef3
tienfuc <tienfu.c@gmail.com>
2015-12-28 11:36:16 +0800

    pep8 on base.py

--- gdcmdtools/base.py
+++ gdcmdtools/base.py
@@ -10,15 +10,15 @@ import httplib2
 import pprint
 import re
 
-import logging 
+import logging
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 BASE_INFO = {
-        "app":"gdcmdtools",
-        "description":'Google Drive command line tools',
-        "version":'1.03'}
+    "app": "gdcmdtools",
+    "description": 'Google Drive command line tools',
+    "version": '1.03'}
 
 GDAPI_VER = 'v2'
 FTAPI_VER = 'v1'
@@ -26,7 +26,9 @@ DISCOVERY_URL = "https://www.googleapis.com/discovery/v1/apis/{api}/{apiVersion}
 
 DEBUG_LEVEL = ('debug', 'info', 'warning', 'error', 'critical')
 
+
 class GDBase(object):
+
     def __init__(self):
         self.drive_service = None
         self.ft_service = None
@@ -34,48 +36,54 @@ class GDBase(object):
         self.root_folder = None
 
     def get_root(self):
-        if self.root_folder == None:
-            if self.drive_service == None:
+        if self.root_folder is None:
+            if self.drive_service is None:
                 self.get_drive_service()
             about = self.drive_service.about().get().execute()
-       
+
         self.root_folder = about['rootFolderId']
         logger.debug("root_folder=%s" % self.root_folder)
         return self.root_folder
 
     def get_drive_service(self, http):
-        self.drive_service = build('drive', GDAPI_VER, 
-            discoveryServiceUrl=DISCOVERY_URL, http=http)
+        self.drive_service = build(
+            'drive',
+            GDAPI_VER,
+            discoveryServiceUrl=DISCOVERY_URL,
+            http=http)
         return self.drive_service
 
     def get_ft_service(self, http):
-        self.ft_service = build('fusiontables', FTAPI_VER, 
-            discoveryServiceUrl=DISCOVERY_URL, http=http)
+        self.ft_service = build('fusiontables', FTAPI_VER,
+                                discoveryServiceUrl=DISCOVERY_URL, http=http)
         return self.ft_service
-    
+
     @staticmethod
     def get_id_from_url(url):
         # normal, https://script.google.com/d/XXXXXXXXX/edit?usp=sharing
         normal_re = "^.*/d/([\w\-]*)"
-        # folder on drive url: https://drive.google.com/drive/u/0/folders/XXXXXXXXXXX
+        # folder on drive url:
+        # https://drive.google.com/drive/u/0/folders/XXXXXXXXXXX
         folder_url_re = "^.*/folders/([\w\-]*)"
-        # folder, https://drive.google.com/folderview?id=XXXXXXXX&usp=sharing 
+        # folder, https://drive.google.com/folderview?id=XXXXXXXX&usp=sharing
         folder_share_re = "^.*/folderview\?id=([\w\-]*)"
         # open by id, https://drive.google.com/open?id=XXXXXXXXXX
         openbyid_re = "^.*/open\?id=([\w\-]*)"
-        # webContentLink, https://docs.google.com/uc?id=XXXXXXXXXX&export=download
+        # webContentLink,
+        # https://docs.google.com/uc?id=XXXXXXXXXX&export=download
         webcontent_re = "^.*/uc\?id=([\w\-]*)"
-        # export link, https://docs.google.com/feeds/download/documents/export/Export?id=XXXXXX&exportFormat=html
+        # export link,
+        # https://docs.google.com/feeds/download/documents/export/Export?id=XXXXXX&exportFormat=html
         export_re = "^.*/[Ee]xport\?id=([\w\-]*)"
 
         # general id as parameter
         id_parameter_re = "^.*\?id=([\w\-]*)"
 
         final_re = r"%s|%s|%s" % \
-                (normal_re, folder_url_re, id_parameter_re)
-                #(normal_re, folder_share_re, folder_url_re, openbyid_re, webcontent_re, export_re)
+            (normal_re, folder_url_re, id_parameter_re)
+        #(normal_re, folder_share_re, folder_url_re, openbyid_re, webcontent_re, export_re)
         search = re.search(final_re, url)
-        
+
         if(search):
             file_id = next(x for x in search.groups() if x is not None)
         else:
