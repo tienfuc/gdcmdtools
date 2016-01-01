d30a4d1a66356897e696d86ee9ab85bf953998ff
tienfuc <tienfu.c@gmail.com>
2016-01-01 16:39:50 +0800

    pumps version to v1.06

--- gdcmdtools/base.py
+++ gdcmdtools/base.py
@@ -18,7 +18,7 @@ logger.setLevel(logging.INFO)
 BASE_INFO = {
     "app": "gdcmdtools",
     "description": 'Google Drive command line tools',
-    "version": '1.05'}
+    "version": '1.06'}
 
 GDAPI_VER = 'v2'
 FTAPI_VER = 'v1'
